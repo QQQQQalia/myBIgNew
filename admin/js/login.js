940655ca6f8aee630981e87688f410fa5466f35f
QQQQQalia <aliatghykk12@gmail.com>
2020-06-22 09:31:14 +0800

    2020-06-22 登录页面完成

--- admin/js/login.js
+++ admin/js/login.js
@@ -2,13 +2,17 @@
  * @Author: QLJ
  * @Date: 2020-06-18 16:18:30
  * @LastEditors: QLJ
- * @LastEditTime: 2020-06-21 17:04:57
+ * @LastEditTime: 2020-06-22 09:30:20
  * @FilePath: \BigNews\admin\js\login.js
  */
 $(function () {
   $('.loginBtn').on('click', function () {
-    let username = $('.input_txt').val()
-    let password = $('.input_pass').val()
+    let username = $('.input_txt').val().trim()
+    let password = $('.input_pass').val().trim()
+    if (username == '' || password == '') {
+      $('#myModal').modal('show');
+      $('.modaltext').text('输入密码或用户名不能为空');
+    }
     // 用户登录
     // 请求地址：/admin/user/login
     // 请求方式：post
@@ -23,11 +27,25 @@ $(function () {
       success: function (response) {
         console.log(response);
         if (response.code == 200) {
-          location.href = './index.html'
+          $('#myModal').modal('show')
+          $('.modaltext').text(response.msg);
+          // hidden.bs.modal监听成功登录后的确认事件
+          // 在模态框关闭后 进行token存储 并跳转到首页
+          $('#myModal').on('hidden.bs.modal', function (e) {
+            // 跳转之前进行token存储
+            localStorage.setItem('BigNewToken', response.token)
+            location.href = './index.html'
+          })
+        } else {
+          $('#myModal').modal('show')
+          $('.modaltext').text(response.msg);
         }
       }
     });
+  });
 
-
+  $('.btnconfirm').click(function (e) {
+    e.preventDefault();
+    $('#myModal').modal('hide')
   });
 })
